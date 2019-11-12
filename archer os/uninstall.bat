8e634e5348d833ca68267c8658b0398cc669c53b
archermancool <57549000+archermancool@users.noreply.github.com>
2019-11-11 18:02:19 -0600

    site

--- archer os/uninstall.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Are you sure you want to uninstall?
-pause
-del "%userprofile%\desktop\ArcherOS.bat"
- del "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ArcherOS.bat"
-@RD /S /Q "c:\ArcherOS"
-cls
-echo uninstall complete!
-pause
